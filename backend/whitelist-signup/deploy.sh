1a319148b090fe09faa1e32cb5d3d30124629625
Ladislav Martincik <ladislav.martincik@gmail.com>
2022-01-31 14:29:28 +0100

    Add production ready whitelist-signup stack

--- backend/whitelist-signup/deploy.sh
+++ backend/whitelist-signup/deploy.sh
@@ -5,7 +5,7 @@ echo "Stage: ${STAGE}"
 # serverless deploy --config resources.yml -s "${STAGE}"
 # serverless deploy --config triggers.yml -s "${STAGE}"
 
-config=$(serverless info --config resources.yml --verbose)
+config=$(serverless info --config resources.yml -s ${STAGE} --verbose)
 echo $config | grep -o 'UserPoolId: [^\ ]*' | grep -o '[^\ ]*$' > .userpoolid
 echo $config | grep -o 'UserPoolWebClient: [^\ ]*' | grep -o '[^\ ]*$' > .userpoolwebclient
 
