fe972fa0c629aaa722209cd09c44dadedf4a146a
Ladislav Martincik <ladislav.martincik@gmail.com>
2022-01-31 11:56:04 +0100

    Add menu to header and footer

--- web/src/partials/Footer.js
+++ web/src/partials/Footer.js
@@ -1,33 +1,39 @@
 import React from 'react';
+import { AcademicCapIcon, ArchiveIcon, ChatIcon, AtSymbolIcon } from '@heroicons/react/outline';
 
 function Footer() {
   return (
-    <footer className="relative">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="py-12 md:py-16 -mt-px">
-          {/* Bottom area */}
-          <div className="md:flex md:items-center md:justify-between">
-            {/* Social links */}
-            <ul className="flex mb-4 md:order-2 md:ml-4 md:mb-0">
-              <li>
-                <a
-                  className="flex justify-center items-center text-white bg-gray-500 dark:text-gray-500 dark:bg-gray-800 hover:underline hover:bg-purple-600 rounded-full transition duration-150 ease-in-out"
-                  href="https://twitter.com/nomaclub"
-                  rel="noreferrer"
-                  target="_blank"
-                  aria-label="Twitter"
-                >
-                  <svg className="w-8 h-8 fill-current" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M24 11.5c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4 0 1.6 1.1 2.9 2.6 3.2-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H8c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4c.7-.5 1.3-1.1 1.7-1.8z" />
-                  </svg>
-                </a>
-              </li>
-            </ul>
-
-            {/* Copyrights note */}
-            <div className="text-gray-200 text-sm mr-4">All rights reserved &copy; 2022 NoMA team</div>
-          </div>
-        </div>
+    <footer className="max-w-6xl mx-auto">
+      <div className="max-w-6xl py-12 md:py-16 -mt-px mx-auto">
+          <ul className="flex justify-end flex-grow items-center">
+            <li>
+              <a href="#" target="_blank" rel="noreferrer" className="text-yellow-400 font-bold ml-6">
+                <AcademicCapIcon className="h-5 w-5 mr-2 inline" />
+                Whitepaper
+              </a>
+            </li>
+            <li>
+              <a href="https://delightful-octopus-60f.notion.site/NoMA-Wiki-138fcb2696b342589d3ac8131ce7b2f2" target="_blank" rel="noreferrer" className="text-white font-bold ml-6">
+                <ArchiveIcon className="h-5 w-5 mr-2 inline" />
+                Wiki
+              </a>
+            </li>
+            <li>
+              <a href="https://discord.gg/qJDaaYBAkF" target="_blank" rel="noreferrer" className="text-white font-bold ml-6">
+                <ChatIcon className="h-5 w-5 mr-2 inline" />
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/nomaclub" target="_blank" rel="noreferrer" className="text-white font-bold ml-6">
+                <AtSymbolIcon className="h-5 w-5 mr-1 inline" />
+                Twitter
+              </a>
+            </li>
+          </ul>
+      </div>
+      <div className="text-gray-200 text-center text-sm mr-4 mb-20">
+        All rights reserved &copy; 2022 NoMA team
       </div>
     </footer>
   );
--- web/src/partials/Header.js
+++ web/src/partials/Header.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { AcademicCapIcon, ArchiveIcon, ChatIcon, AtSymbolIcon } from '@heroicons/react/outline';
 
 function Header() {
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
@@ -40,12 +41,26 @@ function Header() {
           <nav className="hidden md:flex md:flex-grow">
             <ul className="flex justify-end flex-grow items-center">
               <li>
+                <a href="#" target="_blank" rel="noreferrer" className="text-yellow-400 font-bold ml-6">
+                  <AcademicCapIcon className="h-5 w-5 mr-2 inline" />
+                  Whitepaper
+                </a>
+              </li>
+              <li>
+                <a href="https://delightful-octopus-60f.notion.site/NoMA-Wiki-138fcb2696b342589d3ac8131ce7b2f2" target="_blank" rel="noreferrer" className="text-white font-bold ml-6">
+                  <ArchiveIcon className="h-5 w-5 mr-2 inline" />
+                  Wiki
+                </a>
+              </li>
+              <li>
                 <a href="https://discord.gg/qJDaaYBAkF" target="_blank" rel="noreferrer" className="text-white font-bold ml-6">
+                  <ChatIcon className="h-5 w-5 mr-2 inline" />
                   Discord
                 </a>
               </li>
               <li>
                 <a href="https://twitter.com/nomaclub" target="_blank" rel="noreferrer" className="text-white font-bold ml-6">
+                  <AtSymbolIcon className="h-5 w-5 mr-1 inline" />
                   Twitter
                 </a>
               </li>
