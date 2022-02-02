390b0fcb663325e2aa3db6017d22b1ef74b71216
Ladislav Martincik <ladislav.martincik@gmail.com>
2022-02-02 15:04:06 +0100

    Add extended team of Amenti Studio

--- web/src/partials/Team.js
+++ web/src/partials/Team.js
@@ -12,7 +12,7 @@ function Team() {
         <div className="col-span-3 text-left sm:mb-10 md:mb-0">
           <img className="mb-4 sm:mx-auto md:mx-0" src={require('../images/team/mummy-ladi.png')} alt="Ladi / Developer" />
           <div className="w-20 h-20 -mt-14 mb-5 rounded-full overflow-hidden">
-            <img src={require('../images/team/real/ladi.png')} alt="Profile picture of Ladi" className="w-full h-full object-cover" />
+            <img src={require('../images/team/real/ladi.png')} alt="Ladi" className="w-full h-full object-cover" />
           </div>
           <h3 className="h3 sm:text-xl sm:text-center md:text-left mb-2 font-red-hat-display font-extrabold" data-aos="fade-down">
             Ladi / Developer
@@ -24,7 +24,7 @@ function Team() {
         <div className="col-span-3 text-left">
           <img className="mb-4 sm:mx-auto md:mx-0" src={require('../images/team/mummy-jan.png')} alt="Jan - Design" />
           <div className="w-20 h-20 -mt-14 mb-5 rounded-full overflow-hidden">
-            <img src={require('../images/team/real/jan.png')} alt="Profile picture of Jan" className="w-full h-full object-cover" />
+            <img src={require('../images/team/real/jan.png')} alt="Jan" className="w-full h-full object-cover" />
           </div>
           <h3 className="h3 sm:text-xl sm:text-center md:text-left mb-2 font-red-hat-display font-extrabold" data-aos="fade-down">
             Jan / Design
@@ -36,7 +36,7 @@ function Team() {
         <div className="col-span-3 text-left">
           <img className="mb-4 sm:mx-auto md:mx-0" src={require('../images/team/mummy-simon.png')} alt="Šimon - Illustrator" />
           <div className="w-20 h-20 -mt-14 mb-5 rounded-full overflow-hidden">
-            <img src={require('../images/team/real/simon.png')} alt="Profile picture of Simon" className="w-full h-full object-cover" />
+            <img src={require('../images/team/real/simon.png')} alt="Simon" className="w-full h-full object-cover" />
           </div>
           <h3 className="h3 sm:text-xl sm:text-center md:text-left mb-2 font-red-hat-display font-extrabold" data-aos="fade-down">
             Simon / Illustrator
@@ -48,7 +48,7 @@ function Team() {
         <div className="col-span-3 text-left">
           <img className="mb-4 sm:mx-auto md:mx-0" src={require('../images/team/mummy-guide.png')} alt="Mummy Boy - Guide" />
           <div className="w-20 h-20 -mt-14 mb-5 rounded-full overflow-hidden bg-white">
-            <img src={require('../images/team/real/mummy.png')} alt="Profile picture of Jan" className="w-full h-full object-cover" />
+            <img src={require('../images/team/real/mummy.png')} alt="Jan" className="w-full h-full object-cover" />
           </div>
           <h3 className="h3 sm:text-xl sm:text-center md:text-left mb-2 font-red-hat-display font-extrabold" data-aos="fade-down">
             Mummy Boy / Guide
@@ -56,6 +56,62 @@ function Team() {
           <p className="text-gray-400 sm:text-center sm:mx-20 md:text-left md:mx-0">
             Every great project needs a mascot. Mummy boy will be your guide in the fascinating and constantly evolving world of the Metaverse.
           </p>
+        </div>
+      </div>
+      <div className="md:grid md:grid-cols-12 gap-6 mt-20">
+        <div className="col-span-2 text-center sm:mb-10 md:mb-0">
+          <div className="w-20 h-20 rounded-full overflow-hidden mx-auto">
+            <img src={require('../images/team/real/marek.png')} alt="Marek" className="w-full h-full object-cover" />
+          </div>
+          <h3 className="h3 sm:text-xl text-center mt-2 font-red-hat-display font-bold" data-aos="fade-down">
+            Marek
+          </h3>
+          <p>Developer</p>
+        </div>
+        <div className="col-span-2 text-center sm:mb-10 md:mb-0">
+          <div className="w-20 h-20 rounded-full overflow-hidden mx-auto">
+            <img src={require('../images/team/real/kristyna.png')} alt="Kristyna" className="w-full h-full object-cover" />
+          </div>
+          <h3 className="h3 sm:text-xl text-center mt-2 font-red-hat-display font-bold" data-aos="fade-down">
+            Kristyna
+          </h3>
+          <p>Concept Art</p>
+        </div>
+        <div className="col-span-2 text-center sm:mb-10 md:mb-0">
+          <div className="w-20 h-20 rounded-full overflow-hidden mx-auto">
+            <img src={require('../images/team/real/tomas.png')} alt="Tomas" className="w-full h-full object-cover" />
+          </div>
+          <h3 className="h3 sm:text-xl text-center mt-2 font-red-hat-display font-bold" data-aos="fade-down">
+            Tomas
+          </h3>
+          <p>Music</p>
+        </div>
+        <div className="col-span-2 text-center sm:mb-10 md:mb-0">
+          <div className="w-20 h-20 rounded-full overflow-hidden mx-auto">
+            <img src={require('../images/team/real/honza.png')} alt="Honza" className="w-full h-full object-cover" />
+          </div>
+          <h3 className="h3 sm:text-xl text-center mt-2 font-red-hat-display font-bold" data-aos="fade-down">
+            Honza
+          </h3>
+          <p>SFX</p>
+        </div>
+        <div className="col-span-2 text-center sm:mb-10 md:mb-0">
+          <div className="w-20 h-20 rounded-full overflow-hidden mx-auto">
+            <img src={require('../images/team/real/lukas.png')} alt="Lukas" className="w-full h-full object-cover" />
+          </div>
+          <h3 className="h3 sm:text-xl text-center mt-2 font-red-hat-display font-bold" data-aos="fade-down">
+            Lukas
+          </h3>
+          <p>3D &amp; VFX</p>
+        </div>
+        <div className="col-span-2 text-center sm:mb-10 md:mb-0">
+          <div className="w-20 h-20 rounded-full overflow-hidden mx-auto">
+            <img src={require('../images/team/real/davidtomas.png')} alt="David Tomas" className="w-full h-full object-cover" />
+          </div>
+          <h3 className="h3 sm:text-xl text-center mt-2 font-red-hat-display font-bold" data-aos="fade-down">
+            David Tomas
+          </h3>
+          <p>Game Design</p>
         </div>
       </div>
     </section>
