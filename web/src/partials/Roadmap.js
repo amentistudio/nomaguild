bebcca82e5b5bf7a0e341b45763caceebaf15052
Ladislav Martincik <ladislav.martincik@gmail.com>
2022-02-04 13:32:43 +0100

    Fix roadmap responsivity version

--- web/src/partials/Roadmap.js
+++ web/src/partials/Roadmap.js
@@ -20,7 +20,86 @@ function Roadmap() {
 
       <div className="max-w-6xl mx-auto mt-20" style={{ borderBottom: '2px solid #FCD34D', width: 400 }}></div>
 
-      <div className="max-w-4xl lg:mx-auto md:mx-10">
+      <div className="max-w-4xl mx-auto md:mx-10 md:hidden">
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center mt-8 mb-2 font-red-hat-display font-extrabold">
+            Core Team Est.
+            <span className="ml-4 inline-flex items-center px-3 py-1 border border-transparent text-xs font-medium rounded-full shadow-sm text-black bg-green-400">DONE</span>
+          </h3>
+          <p className="text-gray-400 text-center md:text-left sm:mx-20 md:mx-0">
+            We have spent the last year preparing this project and studying all the possibilities of a blockchain.
+            We have found alternative ways of using a blockchain to fund small and medium-sized game developers.
+          </p>
+        </div>
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center mt-8 mb-2 text-left font-red-hat-display font-extrabold">
+            Secure funding
+            <span className="ml-4 inline-flex items-center px-3 py-1 border border-transparent text-xs font-medium rounded-full shadow-sm text-black bg-green-400">DONE</span>
+          </h3>
+          <p className="text-gray-400 text-center md:text-left mx-20">
+            The core team of 8 people is now fully funded thanks to the investment in DeFi.
+            All members can use NoMA Club as a blueprint to start their communities.
+          </p>
+        </div>
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center md:text-left mt-8 mb-2 font-red-hat-display font-extrabold">
+            Funds expansion
+            <span className="ml-4 inline-flex items-center px-3 py-1 border border-transparent text-xs font-medium rounded-full shadow-sm text-black bg-yellow-300">NEXT</span>
+          </h3>
+          <p className="text-gray-400 text-center mx-20">
+            With NFT mint we’ll increase our capital. 20% of the capital will be reserved and sent to community wallet as a foundation for the DAO.
+          </p>
+        </div>
+        <div className="mb-20">
+          <h3 className="h3 text-2xl text-center md:text-left mt-8 mb-2 text-left font-red-hat-display font-extrabold">
+            Community Teams
+          </h3>
+          <p className="text-gray-400 text-center md:text-left sm:mx-20 md:mx-0">
+            We're building a passionate community around gaming and making games. We have zero tolerance for toxic behaviour.
+            We are going to expand the core team with the new members recruited from the community.
+          </p>
+        </div>
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center mt-8 mb-2 text-left font-red-hat-display font-extrabold">
+            NoMA DAO &amp; Treasury
+          </h3>
+          <p className="text-gray-400 text-center mx-20">
+            By placing your mummy in the sarcophagus (burning the token) you can claim the NoMA Founders Token,
+            which guarantees you a share in the NoMA Treasury and also voting right in the community.
+          </p>
+        </div>
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center mt-8 mb-2 text-left font-red-hat-display font-extrabold">
+            Community Projects
+          </h3>
+          <p className="text-gray-400 text-center mx-20">
+            With established community we can start building ROI Projects &amp; start making games together.
+            The Core Team will shift its focus to creating tokenomics and preparing for complete decentralization.
+          </p>
+        </div>
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center mt-8 mb-2 text-left font-red-hat-display font-extrabold">
+            Tokenomics
+          </h3>
+          <p className="text-gray-400 text-center mx-20">
+            The Core Team is working to create an economy that rewards active members.
+            We will also make it possible to fund community projects from the community treasury.
+            All projects must be agreed upon and voted on by NoMA Founders members.
+          </p>
+        </div>
+        <div className="mt-20">
+          <h3 className="h3 text-2xl text-center md:text-left mt-8 mb-2 text-left font-red-hat-display font-extrabold">
+            Decentralization
+          </h3>
+          <ul className="md:list-disc text-gray-400 text-center mx-20 md:ml-5 md:mr-0">
+            <li>Core Team hands reins to the community.</li>
+            <li>Tokens are activated and distributed to active community members.</li>
+            <li>Community control via voting.</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="max-w-4xl lg:mx-auto md:mx-10 sm:hidden md:block">
         <div className="md:grid md:grid-cols-12">
           <div
             className="col-start-1 col-end-7 md:pt-20 md:pr-20"
