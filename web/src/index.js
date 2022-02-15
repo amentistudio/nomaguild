3faf5b9b0a7f2b504762abd77a6534226f49cbdf
Ladislav Martincik <ladislav.martincik@gmail.com>
2022-02-15 14:20:05 +0100

    We need hash router for GH pages

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,7 +9,7 @@ ReactGA.initialize('G-J6M4SL768E');
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename='/'>
       <App />
     </Router>
   </React.StrictMode>,
